f5b92f9d00c38b6b4b0fd6ffd8a292b2778ca024
N.D. Bhavana <ndbhavanastarts@gmail.com>
2023-09-19 20:57:46 -0700

    square path

--- Pair Prototype/Assets/Scripts-Enemies/Enemycircle.cs
+++ Pair Prototype/Assets/Scripts-Enemies/Enemycircle.cs
@@ -4,23 +4,61 @@ using UnityEngine;
 
 public class Enemycircle : MonoBehaviour
 {
-    public float amplitude=5;
-    public float frequency=5;
-    public float xoffset=1;
-    public float zoffset=1;
-    
+    public Vector3 posn1;
+
+    public Vector3 posn2;
+    public Vector3 posn3;
+    public Vector3 posn4;
+    public float speed;
+    public float step;
+    public int ink = 1;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Mathf.Cos(Time.time * frequency)*amplitude;
-        float y = transform.position.y;
-        float z = Mathf.Sin(Time.time * frequency) * amplitude;
-        transform.position = new Vector3(x + xoffset,y,z + zoffset);
+        step = Time.deltaTime * speed;
+
+        if (transform.position == posn1)
+        {
+
+            ink = 1;
+
+        }
+        if (transform.position == posn2)
+        {
+            ink = 2;
+
+        }
+        if (transform.position == posn3)
+        {
+            ink = 3;
+
+        }
+        if (transform.position == posn4)
+        {
+            ink = 4;
+
+        }
+
+        switch (ink)
+        {
+            case 1:
+                transform.position = Vector3.MoveTowards(transform.position, posn2, step);
+                break;
+            case 2:
+                transform.position = Vector3.MoveTowards(transform.position, posn3, step);
+                break;
+            case 3:
+                transform.position = Vector3.MoveTowards(transform.position, posn4, step);
+                break;
+            default:
+                transform.position = Vector3.MoveTowards(transform.position, posn1, step);
+                break;
+        }
     }
 }
